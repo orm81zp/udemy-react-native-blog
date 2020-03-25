12732bfdc8fc689cd8a0756ecaa7ae681d70ca10
Roman Onishchenko <orm81zp@gmail.com>
2020-03-25 10:10:44 +0200

    blogContext

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,23 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext);
+  const { data, addBlogPost } = useContext(BlogContext);
 
   return (
     <View>
       <Text>IndexScreen</Text>
+      <Button title="Add Post" onPress={addBlogPost} />
+      <FlatList
+        data={data}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => {
+          return (
+            <Text>{item.title}</Text>
+          )
+        }}
+      />
     </View>
   );
 };
