7e750c02233b9ae6a1bc11d5d5c9aee298a31165
Roman Onishchenko <orm81zp@gmail.com>
2020-03-26 17:08:48 +0200

    showScreen

---
+++ src/screens/ShowScreen.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const ShowScreen = ({ navigation }) => {
+  const { state } = useContext(Context);
+  const blogPostId = navigation.getParam('id');
+  const blogPost = state.find((blogPost) => blogPost.id === blogPostId);
+
+  return (
+    <View>
+      <Text>{blogPost.title}</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({});
+
+export default ShowScreen;
